c041b36d44b332c4ef3e4630527a8411d9a6bbf9
Ekam978 <73385604+Ekam978@users.noreply.github.com>
2021-09-03 15:19:40 +0530

    Update news1.js

--- news1.js
+++ news1.js
@@ -1,14 +1,10 @@
 function get_data() {
-    fetch('https://newsapi.org/v2/everything?q=climate&sortBy=publishedAt&apiKey=f4a7cc07790747a88ad8519c05a8e531')
-    .then(a => a.json())
-    .then(response =>{
-        console.log(response);
-        length=response.articles.length;
-        console.log(length)
-        for(var i=0; i<response.articles.length; i++){
-            document.getElementById("container").innerHTML+="<div id='news'><img src='"+response.articles[i].urlToImage+"'><h3>"+response.articles[i].title+"</h3><br>"+response.articles[i].description+"</div><hr>"
+            fetch('http://newsapi.org/v2/everything?q=climate&sortBy=publishedAt&apiKey=f4a7cc07790747a88ad8519c05a8e531')
+            .then(a => a.json())
+            .then(response => {
+                for (i =0 ; i<response.articles.length ; i++){
+                    document.getElementById("container").innerHTML+="<div id='news'><img src='"+response.articles[i].urlToImage+"'><h3>"+response.articles[i].title+"</h3><br><h4>"+response.articles[i].description+"</h4><hr>"
+                }
+            })
         }
-    })
-}
-
-get_data();
+        get_data();
